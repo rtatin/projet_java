116387a11007fbf3b19fb9079c10ecfca4079490
Tatin <rtwam@hotmail.fr>
2019-11-04 14:59:10 +0100

    match

--- src/project_football_simulator/Match.java
+++ src/project_football_simulator/Match.java
@@ -22,15 +22,32 @@ public class Match {
         this.ref=ref;
     }
     
-    public int playersOnField(Team team){
-    }
+    
     public float atkTeam(int numberTeam){
+        float totalAtk=0;
         for(int i=0; i<10;i++){
-            if (super.teams[numberTeam].players
+            Player play=teams[numberTeam].players.get(i);
+            if (play.field==true){
+                totalAtk+=play.attack;
+            }
         }
+        return totalAtk;
     }
+    
+    public float defTeam(int numberTeam){
+        float totalDef=0;
+        for(int i=0; i<10;i++){
+            Player play=teams[numberTeam].players.get(i);
+            if (play.field==true){
+                totalDef+=play.defense;
+            }
+        }
+        return totalDef;
+    }
+    
     public void playingMatch(){
         this.time+=15;
-        float atkTeam1=0;
+        float atkTeam1=atkTeam(0);
+        float atkTeam2=atkTeam(1);
     }
 }
