b8e080f2f5e46aef604799ac63e0170822f89c19
theophile <theophilevonck@gmail.com>
2019-11-07 10:38:44 +0100

    Auto stash before merge of "java" and "origin/java"

--- src/project_football_simulator/Manager.java
+++ src/project_football_simulator/Manager.java
@@ -5,6 +5,8 @@
  */
 package project_football_simulator;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rtwam
@@ -18,5 +20,27 @@ public class Manager extends Character {
         super.age=age;
         super.nationality=nationality;
         super.equipe=equipe;
+    }
+    public void teamManagement(){
+        System.out.println("select the boost: \n enter 1 to boost attack\n enter 2 to boost middle\n enter 3 to boost defense");
+        Scanner choice=new Scanner(System.in);
+        String obj = choice.nextLine();
+        int choosenboost=(Integer.parseInt(obj));
+        
+        switch(choosenboost) {
+            case 1:
+                this.boostAttack=this.attack+this.attack*(cont/10);
+                break;
+            case 2:
+                this.boostAttack=this.attack+this.attack*(cont/20);
+                this.boostDefence=this.defense+this.defense*(cont/20);
+                break;
+            case 3:
+                this.boostDefence=this.defense+this.defense*(cont/10);
+                break;
+            default:
+                break;
+            }  
+    
     }
 }
